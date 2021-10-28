de189b8b5c6205cd0757173aeb95a0526fcebf12
Lucas Feliciano <lucas1feliciano0@gmail.com>
2021-10-28 20:03:57 -0300

    Implementada splash screen;

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from 'styled-components/native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import RNBootSplash from 'react-native-bootsplash';
 
 import HomeTabs from '@routes/HomeTabs';
 
@@ -11,6 +12,9 @@ import theme from '@theme/index';
 import store, {persistor} from './store';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true});
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
